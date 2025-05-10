33893a40b5bd2a64b706e6781958746f9696f348
Jason King <jason@initiateit.com.au>
2025-05-10 19:43:25 +0930

    Add fan speed support

--- nvsmi.py
+++ nvsmi.py
@@ -23,7 +23,7 @@ import sys
 __version__ = "0.4.2"
 
 
-NVIDIA_SMI_GET_GPUS = "nvidia-smi --query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode,temperature.gpu --format=csv,noheader,nounits"
+NVIDIA_SMI_GET_GPUS = "nvidia-smi --query-gpu=index,uuid,fan.speed,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode,temperature.gpu --format=csv,noheader,nounits"
 NVIDIA_SMI_GET_PROCS = "nvidia-smi --query-compute-apps=pid,process_name,gpu_uuid,gpu_name,used_memory --format=csv,noheader,nounits"
 
 
@@ -32,6 +32,7 @@ class GPU(object):
         self,
         id,
         uuid,
+        fan_speed,
         gpu_util,
         mem_total,
         mem_used,
@@ -45,8 +46,9 @@ class GPU(object):
     ):
         self.id = id
         self.uuid = uuid
+        self.fan_speed = fan_speed  # ← FIXED THIS LINE
         self.gpu_util = gpu_util
-        self.mem_util = float(mem_used) / float(mem_total) * 100
+        self.mem_util = float(mem_used) / float(mem_total) * 100 if mem_total else 0
         self.mem_total = mem_total
         self.mem_used = mem_used
         self.mem_free = mem_free
@@ -58,7 +60,7 @@ class GPU(object):
         self.temperature = temperature
 
     def __repr__(self):
-        msg = "id: {id} | UUID: {uuid} | gpu_util: {gpu_util:5.1f}% | mem_util: {mem_util:5.1f}% | mem_free: {mem_free:7.1f}MB |  mem_total: {mem_total:7.1f}MB"
+        msg = "id: {id} | UUID: {uuid} | fan: {fan_speed:5.1f}% | gpu_util: {gpu_util:5.1f}% | mem_util: {mem_util:5.1f}% | mem_free: {mem_free:7.1f}MB |  mem_total: {mem_total:7.1f}MB"
         msg = msg.format(**self.__dict__)
         return msg
 
@@ -96,19 +98,21 @@ def _get_gpu(line):
     values = line.split(", ")
     id = values[0]
     uuid = values[1]
-    gpu_util = to_float_or_inf(values[2])
-    mem_total = to_float_or_inf(values[3])
-    mem_used = to_float_or_inf(values[4])
-    mem_free = to_float_or_inf(values[5])
-    driver = values[6]
-    gpu_name = values[7]
-    serial = values[8]
-    display_active = values[9]
-    display_mode = values[10]
-    temp_gpu = to_float_or_inf(values[11])
+    fan_speed = to_float_or_inf(values[2])
+    gpu_util = to_float_or_inf(values[3])
+    mem_total = to_float_or_inf(values[4])
+    mem_used = to_float_or_inf(values[5])
+    mem_free = to_float_or_inf(values[6])
+    driver = values[7]
+    gpu_name = values[8]
+    serial = values[9]
+    display_active = values[10]
+    display_mode = values[11]
+    temp_gpu = to_float_or_inf(values[12])
     gpu = GPU(
         id,
         uuid,
+        fan_speed,
         gpu_util,
         mem_total,
         mem_used,
@@ -121,6 +125,7 @@ def _get_gpu(line):
         temp_gpu,
     )
     return gpu
+
 
 
 def get_gpus():
